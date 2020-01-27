8b5cb74336294f1c27e393706a86eb2608fdda82
Pierluc Dupont <pierlucdupont01@hotmail.ca>
2020-01-27 14:39:52 -0500

    Exercice2complete

---
+++ exerciseWeek2/exerciseWeek2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace exerciseWeek2
+{
+    class Program
+    {
+        public static string Nom = "Pierluc Dupont";
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Nom);
+        }
+    }
+}
