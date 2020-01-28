fd5a2bd4a7c0a0ed1a433b5af28173fd6565a75e
Pierluc Dupont <pierlucdupont01@hotmail.ca>
2020-01-27 19:26:08 -0500

    Exercice2complete

---
+++ exerciseWeek2/exerciseWeek2/num4/num4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace num4
+{
+    class Program
+    {
+        public static int Somme(int[] n)
+        {
+            int total = 0;
+            for(int i=0; i<n.Length; i++)
+            {
+                total += n[i];
+            }
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            int[] nombres = { 10, 11, 5 ,15, 12, 8, 9, 6, 5, 3, 4, 6, 13, 16, 14 };
+
+            Console.WriteLine(Somme(nombres));
+        }
+    }
+}
