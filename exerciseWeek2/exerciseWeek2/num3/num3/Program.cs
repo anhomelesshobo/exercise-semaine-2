f1da50c0ecee6e04b76b9d8e0c157cbd1125691b
Pierluc Dupont <pierlucdupont01@hotmail.ca>
2020-01-27 19:03:53 -0500

    Exercice3complete

---
+++ exerciseWeek2/exerciseWeek2/num3/num3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace num3
+{
+   
+    class Program
+    {
+        public static bool isPrime(int n)
+        {
+            int x = (int)Math.Floor(Math.Sqrt(n));
+
+            if(n==1)
+            {
+                return false;
+            }
+            if(n==2)
+            {
+                return true;
+            }
+            else
+            {
+                int i = 2;
+
+                while(i <= x)
+                {
+                    
+                    if(n%i==0)
+                    {
+                        return false;
+                    }
+                    
+                        i++;
+                    
+                }
+                
+                return true;
+                
+            }
+        }
+        public static void Main()
+        {
+           
+            Console.WriteLine("\nSum of the first 500 prime numbers: ");
+
+            long sum = 0;
+            int ctr = 0;
+            int n = 2;
+
+            while(ctr < 500)
+            {
+                if(isPrime(n))
+                {
+                    sum += n;
+                    ctr++;
+                }
+                n++;
+            }
+
+            Console.WriteLine(sum.ToString());
+        }
+    }
+}
